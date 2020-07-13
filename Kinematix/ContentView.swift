b1bb13f33292f154f5003fc6f7cf0c3ccfd76378
Cathy Chang <cathyy8c3@gmail.com>
2020-07-13 16:27:57 -0700

    Initial Commit

---
+++ Kinematix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kinematix
+//
+//  Created by Cathy Chang on 7/13/20.
+//  Copyright © 2020 Kinematix. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
